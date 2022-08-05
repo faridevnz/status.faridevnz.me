1df3f84f1c0adae0120166b34cab0a0afed08297
faridevnz <alessio.farinelli.developer@gmail.com>
2022-08-05 12:20:12 +0200

    wip

--- app/src/App.js
+++ app/src/App.js
@@ -3,45 +3,23 @@ import { useEffect, useState } from 'react';
 import axios from 'axios';
 
 function App() {
-  const [items, setItems] = useState({}); // { site-name: items }
-  const [parsedItems, setParsedItems] = useState([]) // [{ siteName: name, items: [] }]
   const [sites, setSites] = useState([]);
-
-  useEffect(() => {
-    // fetch items
-    sites.forEach(site => {
-      axios.get(`https://status.faridevnz.me/api/sites/${site.sitename}`).then(res => {
-        setItems((prevItems) => {
-          return { ...prevItems, [site.site]: res.data.ping }
-        })
-      })
-    })
-  }, [sites])
 
   useEffect(() => {
     // take active sites
     axios.get('https://status.faridevnz.me/api/sites').then(res => {
       setSites(res.data);
-    })
-  }, [])
-
-  useEffect(() => {
-    const temp = [];
-    Object.entries(items).forEach(([siteName, items]) => {
-      const a = 50 - items.length;
-      temp.push({ siteName, items: new Array(a).fill({value: 0}).concat(items) })
-    })
-    setParsedItems(temp)
-  }, [items])
+    });
+  }, []);
 
   return (
     <div>
-      { parsedItems.map((siteItem, index) => 
+      { sites.map((site, index) => 
         <div key={index}>
-          <div className='sitename'>{ siteItem.siteName }</div>
+          <div className='sitename'>{ site.sitename } - <a href={ site.site }>{ site.site }</a></div>
           <div className="card" key={index}>
-            { siteItem.items.map((item, index) =>
-              <div className={`item ${item.value === 0 ? 'item--light-grey' : ''} ${item.value > 0.100 ? 'item--orange' : 'item--green'} ${item.value === null ? 'item--red' : ''}`} key={index}></div>
+            { site.tracking.ping.map((ping, index) =>
+              <div className={`item ${ping.value === 0 ? 'item--light-grey' : ''} ${ping.value > 0.100 ? 'item--orange' : 'item--green'} ${ping.value === null ? 'item--red' : ''}`} key={index}></div>
             ) }
           </div>
         </div>
