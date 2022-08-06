ebc75f41694f8510e8527170411f7df6f0f7e234
faridevnz <alessio.farinelli.developer@gmail.com>
2022-08-06 19:35:16 +0200

    wip

--- server/modules/metrics.js
+++ server/modules/metrics.js
@@ -77,7 +77,7 @@ export const cpu_load = () => {
   // return data
   const result = {};
   for ( let i = 0; i < core_number(); i++ ) {
-    const core_number = groups[1 + (i)];
+    const core_number = groups[0 + (i*7)];
     result[core_number] = { user: groups[1 + (i*7)], nice: groups[2 + (i*7)], system: groups[3 + (i*7)], iowait: groups[4 + (i*7)], steal: groups[5 + (i*7)], idle: groups[6 + (i*7)] };
   }
   return result;
