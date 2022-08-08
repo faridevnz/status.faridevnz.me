4e266d8c6cfd53f629df2fdfda920078d9469553
faridevnz <alessio.farinelli.developer@gmail.com>
2022-08-08 14:43:02 +0200

    wip

--- server/server.js
+++ server/server.js
@@ -71,14 +71,14 @@ app.get('/sites/:host/:type/logs', async (req, res) => {
 });
 
 app.get('/metrics', async (req, res) => {
-  const [memory_info, core_num, cpu_specs, average, current, tasks, usage, active_tcp_connections] = await Promise.all([current_memory_info(), core_number(), cpu_specs(), average_cpu_load(), current_cpu_load(), current_running_tasks(), network_usage(), active_tcp_connections()]);
+  const [memory_info, core_num, core_cpu_specs, average, current, tasks, usage, tcp_connections] = await Promise.all([current_memory_info(), core_number(), cpu_specs(), average_cpu_load(), current_cpu_load(), current_running_tasks(), network_usage(), active_tcp_connections()]);
   const { total, ...memory_load } = memory_info;
   res.send({
     cpu: {
       specs: { 
         core_num,
         cores: {
-          ...cpu_specs, 
+          ...core_cpu_specs, 
         },
       },
       load: {
@@ -96,7 +96,7 @@ app.get('/metrics', async (req, res) => {
       tasks,
       network: {
         usage,
-        active_tcp_connections,
+        active_tcp_connections: tcp_connections,
       }
     }
   });
