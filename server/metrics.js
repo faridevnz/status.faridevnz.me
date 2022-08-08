549b22692845e4021a9c162914b802625ff22f9b
faridevnz <alessio.farinelli.developer@gmail.com>
2022-08-08 14:46:51 +0200

    wip

--- server/metrics.js
+++ server/metrics.js
@@ -1,18 +1,19 @@
 import { active_tcp_connections, average_cpu_load, core_number, cpu_specs, current_cpu_load, current_memory_info, current_running_tasks, network_usage } from "./modules/metrics.js";
 
 
-const { total, ...memory_load } = current_memory_info();
-const res = {
+const [memory_info, core_num, core_cpu_specs, average, current, tasks, usage, tcp_connections] = await Promise.all([current_memory_info(), core_number(), cpu_specs(), average_cpu_load(), current_cpu_load(), current_running_tasks(), network_usage(), active_tcp_connections()]);
+const { total, ...memory_load } = memory_info;
+console.log({
   cpu: {
     specs: { 
-      core_num: core_number() ,
+      core_num,
       cores: {
-        ...cpu_specs(), 
+        ...core_cpu_specs, 
       },
     },
     load: {
-      average: average_cpu_load(),
-      current: current_cpu_load()
+      average,
+      current
     },
   },
   ram: {
@@ -22,12 +23,10 @@ const res = {
     load: memory_load,
   },
   stats: {
-    tasks: current_running_tasks(),
+    tasks,
     network: {
-      usage: network_usage(),
-      active_tcp_connections: active_tcp_connections()
+      usage,
+      active_tcp_connections: tcp_connections,
     }
   }
-};
-
-console.log(res);
+});
